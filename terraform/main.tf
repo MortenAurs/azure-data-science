fbabf6ef5ab2104a5ae9c4ab7271a10d79eb381a
BouvetMorten <morten.aursland@bouvet.no>
2022-12-31 00:00:53 +0100

    remove tags

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,9 +27,6 @@ data "azurerm_client_config" "current" {}
 resource "azurerm_resource_group" "this" {
   name     = var.resource_group_name
   location = var.location
-  tags = {
-    "test" = "value"
-  }
 }
 
 resource "azurerm_virtual_network" "this" {
